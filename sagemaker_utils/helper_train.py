22167b25fac3005facfc24e6d864d956a54e1827
sofiacharnota <sofiacharnota@gmail.com>
2025-02-26 14:32:05 +0100

    use tools/train as entry_point

--- sagemaker_utils/helper_train.py
+++ sagemaker_utils/helper_train.py
@@ -1,39 +1,46 @@
 from sagemaker.pytorch import PyTorch
-import boto3
-import os
 
 # Define S3 paths
-s3_scripts = "s3://dd-sample-bucket-north/mmdetection3d/scripts/"
 s3_processed_data = "s3://dd-sample-bucket-north/mmdetection3d/datasets/kitti_processed/"
 s3_model_weights = "s3://dd-sample-bucket-north/mmdetection3d/model_weights/"
 s3_output_model = "s3://dd-sample-bucket-north/mmdetection3d/mmdetection3d-train-output/"
 
-image_uri = "910181886844.dkr.ecr.eu-north-1.amazonaws.com/mmdetection3d:latest"
+# AWS IAM Role & Custom Docker Image
+image_uri = "910181886844.dkr.ecr.eu-north-1.amazonaws.com/mmdetection3d:with-sagemaker-training"
 iam_role = "arn:aws:iam::910181886844:role/AmazonSageMaker-ExecutionRole"
 
-# Create a local directory to store scripts
-LOCAL_SCRIPTS_DIR = "/Users/sofiacharnota/Dev/valohai/mmdetection3d_fork/sagemaker_utils"
-os.makedirs(LOCAL_SCRIPTS_DIR, exist_ok=True)
+# ✅ Git configuration (auto-pulls latest scripts from your repo)
+git_config = {
+    'repo': 'https://github.com/SofiaChar/mmdetection3d.git',
+    'branch': 'sagemaker-integration'
+}
 
-# # Download the training script from S3
-# s3_client = boto3.client("s3")
-# s3_client.download_file("dd-sample-bucket-north", "mmdetection3d/scripts/mvxnet_train.py", f"{LOCAL_SCRIPTS_DIR}/mvxnet_train.py")
+# ✅ Define training arguments
+hyperparameters = {
+    "config_file": "configs/mvxnet_fpn_dv_second_secfpn_8xb2-80e_kitti-3d-3class.py",
+    "work_dir": "/opt/ml/output/data",
+    "resume": "auto",
+    "launcher": "none"
+}
 
-# Define SageMaker Estimator
+# ✅ SageMaker Estimator for Training
 estimator = PyTorch(
-    entry_point="sagemaker_utils/mvxnet_train.py",
-    # source_dir="sagemaker_utils",
+    entry_point="tools/train.py",  # Directly run train.py
+    git_config=git_config,
+    source_dir="../",  # Make sure all scripts from the repo are available
+    dependencies=["configs"],  # Ensure configs folder is included
     role=iam_role,
     instance_count=1,
     instance_type="ml.g4dn.2xlarge",
     image_uri=image_uri,
-    hyperparameters={"epochs": 10},
     output_path=s3_output_model,
-    input_mode="File",  # SageMaker automatically downloads S3 data to /opt/ml/input/data/
-    volume_size_in_gb=300
+    input_mode="File",
+    volume_size_in_gb=300,
+    hyperparameters=hyperparameters  # ✅ Pass arguments dynamically
 )
 
-# Start Training Job
+# ✅ Start Training Job
 estimator.fit({
-    "dataset": s3_processed_data,
+    "kitti": s3_processed_data,
+    "model_weights": s3_model_weights
 })
